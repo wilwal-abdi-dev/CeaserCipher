a8b61eaa1991a641deee8f471c9823f0e2f7f6b9
wiilwaal <suldanwilwal@gmail.com>
2021-08-08 23:45:42 +0300

    decrypt.java

--- src/main/java/Decrypt.java
+++ src/main/java/Decrypt.java
@@ -1,27 +1,27 @@
-//public class Decrypt {
-//
-//    public static StringBuffer decrypting(String cipherText, int shift){
-//
-//        //Decrypt the text
-//        StringBuffer decrypt= new StringBuffer();
-//        for (int j = 0; j < cipherText.length(); j++){
-//
-//            //Check if text is upper case
-//            if (Character.isUpperCase(cipherText.charAt(j))){
-//                char d = (char) (((int)cipherText.charAt(j) - shift -65) % 26 + 65);
-//                decrypt.append(d);
-//
-//                //Check if text is lower case
-//            } else {
-//                char d = (char) (((int)cipherText.charAt(j) - shift -97) % 26 + 97);
-//                decrypt.append(d);
-//
-//            }
-//        }
-//
-//        return decrypt;
-//
-//    }
-//}
-//
-//
+public class Decrypt {
+
+    public static StringBuffer decrypting(String cipherText, int shift){
+
+        //Decrypt the text
+        StringBuffer decrypt= new StringBuffer();
+        for (int j = 0; j < cipherText.length(); j++){
+
+            //Check if text is upper case
+            if (Character.isUpperCase(cipherText.charAt(j))){
+                char d = (char) (((int)cipherText.charAt(j) - shift -65) % 26 + 65);
+                decrypt.append(d);
+
+                //Check if text is lower case
+            } else {
+                char d = (char) (((int)cipherText.charAt(j) - shift -97) % 26 + 97);
+                decrypt.append(d);
+
+            }
+        }
+
+        return decrypt;
+
+    }
+}
+
+
