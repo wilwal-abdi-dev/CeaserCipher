2e3f79a5ccfbf5b836ddca5badadeab602a04cf8
wiilwaal <suldanwilwal@gmail.com>
2021-08-08 23:43:28 +0300

    ceasertest.java

--- src/test/java/CeaserTest.java
+++ src/test/java/CeaserTest.java
@@ -1,25 +1,25 @@
-//import org.junit.Test;
-//
-//
-//import static junit.framework.TestCase.assertEquals;
-//
-//
-//public class CeaserTest {
-//
-//    // test input to be string
-//    @Test
-//    public void  testEncoding_testWord_String(){
-//        Encrypt testEncrypt = new Encrypt();
-//        assertEquals(true , testEncrypt instanceof Encrypt);
-//    }
-//
-//    @Test
-//    public void decryptTest_testWord_String(){
-//
-//        Decrypt decryptTest = new Decrypt();
-//        assertEquals(true, decryptTest instanceof Decrypt);
-//    }
-//}
-//
-//
-//
+import org.junit.Test;
+
+
+import static junit.framework.TestCase.assertEquals;
+
+
+public class CeaserTest {
+
+    // test input to be string
+    @Test
+    public void  testEncoding_testWord_String(){
+        Encrypt testEncrypt = new Encrypt();
+        assertEquals(true , testEncrypt instanceof Encrypt);
+    }
+
+    @Test
+    public void decryptTest_testWord_String(){
+
+        Decrypt decryptTest = new Decrypt();
+        assertEquals(true, decryptTest instanceof Decrypt);
+    }
+}
+
+
+
